5ab5f8eadbe4d2c011597445519ac99cd59793e8
ninuninuninnu <107189450+ninuninuninnu@users.noreply.github.com>
2023-10-24 20:15:59 +0700

    Add files via upload

---
+++ dart3/bin/server.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+
+import 'package:mysql1/mysql1.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
+import 'package:shelf_router/shelf_router.dart';
+
+// Configure routes.
+final _router = Router()
+  ..get('/', _rootHandler)
+  ..get('/echo/<message>', _echoHandler)
+  ..get('/user', _connectSqlHandler);
+
+Future<Response> _connectSqlHandler(Request request) async {
+  var settings = ConnectionSettings(
+      host: 'localhost',
+      port: 3306,
+      user: 'dart2',
+      password: 'password',
+      db: 'vigenesia');
+  var conn = await MySqlConnection.connect(settings);
+
+  var user = await conn.query('SELECT * FROM user', []);
+
+  return Response.ok(user.toString());
+}
+
+Response _rootHandler(Request req) {
+  return Response.ok('Hello, World!\n');
+}
+
+Response _echoHandler(Request request) {
+  final message = request.params['message'];
+  return Response.ok('$message\n');
+}
+
+void main(List<String> args) async {
+  // Use any available host or container IP (usually `0.0.0.0`).
+  final ip = InternetAddress.anyIPv4;
+
+  // Configure a pipeline that logs requests.
+  final handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
+
+  // For running in containers, we respect the PORT environment variable.
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final server = await serve(handler, ip, port);
+  print('Server listening on port ${server.port}');
+}
